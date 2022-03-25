b8925d4968b0d4d046e3c50309d098884e4b7b49
Shamo418 <luxdeus7@gmail.com>
2022-03-25 15:38:04 +0100

    Small changes in functions

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# This is a tic-tac-toe game for two players
+"""This is a tic-tac-toe game for two players"""
 
 
 # Game data
 board = [[' ', ' ', ' '],
          [' ', ' ', ' '],
          [' ', ' ', ' ']]
+
+X, O = 'X', 'O'
 
 
 # Game functions
@@ -24,131 +26,115 @@ def visualize_board():
           f"*\n     *-----*-----*-----*\n ")
 
 
-def check_player(player):
-    """check with player is now playing"""
-    player_name = None
-    if not player:
-        player_name = "Player O"
-    elif player:
-        player_name = "Player X"
-    return player_name
-
-
-def check_input(player_name) -> str:
-    """check if input is in correct format"""
+def proper_answer(player) -> str:
+    """check if input(players move) is in correct format and return player's move"""
     list_of_correct_answers = ['11', '12', '13', '21', '22', '23', '31', '32', '33']
     correct_form = False
     player_move = None
     while not correct_form:
         player_move = input(
-            f"ROW = 1,2,3 COLUMN = 1,2,3\n{player_name}\nPlease input row and column:\n")
+            f"ROW = 1,2,3 COLUMN = 1,2,3\nPlayer {player}\nPlease input row and column:\n")
         correct_form = player_move in list_of_correct_answers
     return player_move
 
 
 def transform_input(player_move):
-    """change a string input to an integer"""
+    """change a string input (player's answer) to an integer used as coordinates for the board"""
     row = int(int(player_move[0]) - 1)
     column = int(int(player_move[1]) - 1)
     return row, column
 
 
-def check_empty_space(player_name, row, column):
-    """check if player's input is in an empty space in the board of game"""
+def empty_space_on_board(player, row, column):
+    """return position of board if player's move is in an empty space"""
     while board[row][column] != ' ':
-        player_mov = check_input(player_name)
-        row, column = transform_input(player_mov)
+        player_move = proper_answer(player)
+        row, column = transform_input(player_move)
         if board[row][column] == ' ':
             return row, column
     return row, column
 
 
-def game_move(player, row, column):
+def set_mark_on_board(player, row, column):
     """place the player's mark ('X' or 'O') on the board"""
-    if player:
+    if player == X:
         if board[row][column] == ' ':
-            board[row][column] = 'X'
-    elif not player:
+            board[row][column] = X
+    elif player == O:
         if board[row][column] == ' ':
-            board[row][column] = 'O'
+            board[row][column] = O
 
 
-def winner_check(x_winner=False, o_winner=False, board_is_full=False):
-    """print which player won"""
-    if x_winner is True:
-        print("Winner is player X")
-    elif o_winner is True:
-        print("Winner is player O")
-    elif board_is_full is True:
+def winner_print(winner, board_is_full, player):
+    """print which player won or if there was a draw"""
+    if winner is True:
+        print(f"Winner is player {player}")
+    elif board_is_full is True and winner is False:
         print("It is Draw")
 
 
-def game_over(x_winner=False, o_winner=False, board_is_full=False):
-    """end of game condition"""
-    if x_winner is True or o_winner is True or board_is_full is True:
+def keep_playing(winner=False, board_is_full=False) -> bool:
+    """checks if there is a game winner or if there is a tie. If it is, it ends the game"""
+    if winner is True or board_is_full is True:
         return False
     else:
         return True
 
 
-def if_x_winner():
-    """check if X player is winner"""
-    x_winner = False
-    if f"{board[0][0]}{board[0][1]}{board[0][2]}" == "XXX" or \
-            f"{board[1][0]}{board[1][1]}{board[1][2]}" == "XXX" or \
-            f"{board[2][0]}{board[2][1]}{board[2][2]}" == "XXX" or \
-            f"{board[0][0]}{board[1][0]}{board[2][0]}" == "XXX" or \
-            f"{board[0][1]}{board[1][1]}{board[2][1]}" == "XXX" or \
-            f"{board[0][0]}{board[1][1]}{board[2][2]}" == "XXX" or \
-            f"{board[0][2]}{board[1][2]}{board[2][2]}" == "XXX" or \
-            f"{board[0][2]}{board[1][1]}{board[2][0]}" == "XXX":
-        x_winner = True
-    return x_winner
+def is_winner(player) -> bool:
+    """return true if player is winner"""
+    winner = False
+    players_marks = None
+    if player == X:
+        players_marks = "XXX"
+    elif player == O:
+        players_marks = "OOO"
+    if f"{board[0][0]}{board[0][1]}{board[0][2]}" == players_marks or \
+            f"{board[1][0]}{board[1][1]}{board[1][2]}" == players_marks or \
+            f"{board[2][0]}{board[2][1]}{board[2][2]}" == players_marks or \
+            f"{board[0][0]}{board[1][0]}{board[2][0]}" == players_marks or \
+            f"{board[0][1]}{board[1][1]}{board[2][1]}" == players_marks or \
+            f"{board[0][0]}{board[1][1]}{board[2][2]}" == players_marks or \
+            f"{board[0][2]}{board[1][2]}{board[2][2]}" == players_marks or \
+            f"{board[0][2]}{board[1][1]}{board[2][0]}" == players_marks:
+        winner = True
+    return winner
 
 
-def if_o_winner():
-    """check if O player is winner"""
-    o_winner = False
-    if f"{board[0][0]}{board[0][1]}{board[0][2]}" == "OOO" or \
-            f"{board[1][0]}{board[1][1]}{board[1][2]}" == "OOO" or \
-            f"{board[2][0]}{board[2][1]}{board[2][2]}" == "OOO" or \
-            f"{board[0][0]}{board[1][0]}{board[2][0]}" == "OOO" or \
-            f"{board[0][1]}{board[1][1]}{board[2][1]}" == "OOO" or \
-            f"{board[0][0]}{board[1][1]}{board[2][2]}" == "OOO" or \
-            f"{board[0][2]}{board[1][2]}{board[2][2]}" == "OOO" or \
-            f"{board[0][2]}{board[1][1]}{board[2][0]}" == "OOO":
-        o_winner = True
-    return o_winner
-
-
-def full_board():
-    """check if a board of game is full"""
+def is_full_board() -> bool:
+    """return True if a board of game is full"""
     board_is_full = False
-    all_pol = [val for sublist in board for val in sublist]
-    if all(n != ' ' for n in all_pol):
+    # Create list of place from the board of game
+    all_position = [position for sublist in board for position in sublist]
+    # Check if all positions are full
+    if all(position != ' ' for position in all_position):
         board_is_full = True
     return board_is_full
 
 
 def main():
     """tic tac toe game"""
-    player_one = True
-    player_two = False
+    player_one = X
+    player_two = O
     game_on = True
     visualize_board()
     intro()
     while game_on:
-        player = check_player(player_one)
-        player_choice = check_input(player)
+        # Start of game by player X, next will be player O
+
+        # Choice of place on the board by player
+        player_choice = proper_answer(player_one)
+        # Transform of choice and add mark on the board
         row, column = transform_input(player_choice)
-        row, column = check_empty_space(player, row, column)
-        game_move(player_one, row, column)
+        row, column = empty_space_on_board(player_one, row, column)
+        set_mark_on_board(player_one, row, column)
         visualize_board()
-        winner_x = if_x_winner()
-        winner_o = if_o_winner()
-        draw = full_board()
-        winner_check(winner_x, winner_o, draw)
-        game_on = game_over(winner_x, winner_o, draw)
+        # Check if there is a winner or a draw
+        winner = is_winner(player_one)
+        draw = is_full_board()
+        winner_print(winner, draw, player_one)
+        game_on = keep_playing(winner, draw)
+        # Change of player ( X - O)
         player_one, player_two = player_two, player_one
 
 
